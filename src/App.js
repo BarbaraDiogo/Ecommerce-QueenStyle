adbd60ea73af28774bd0aa9e4f5438a000f09cfb
Barbara Diogo <barbara2diogo@gmail.com>
2023-11-20 15:16:08 -0300

    Voltando p rota original

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ const Layout = () => {
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route>
-      <Route path="/Ecommerce-QueenStyle" element={<Layout />}>
+      <Route path="/" element={<Layout />}>
         {/* ==================== Header Navlink Start here =================== */}
         <Route index element={<Inicio />}></Route>
         <Route path="/produtos" element={<Produtos />}></Route>
